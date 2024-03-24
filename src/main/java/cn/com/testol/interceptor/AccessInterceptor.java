8b44395e9932a10117d55eeb15efa97c9ace6714
cangling43 <2861268484@qq.com>
2024-03-24 17:36:56 +0800

    更改网址

--- src/main/java/cn/com/testol/interceptor/AccessInterceptor.java
+++ src/main/java/cn/com/testol/interceptor/AccessInterceptor.java
@@ -17,6 +17,7 @@ public class AccessInterceptor implements HandlerInterceptor {
         //response.setCharacterEncoding("UTF-8");
         //response.setContentType("application/json; charset=utf-8");
         response.setHeader("Access-Control-Allow-Credentials","true");
+//        response.setHeader("Access-Control-Allow-Origin", "http://60.205.137.48");
         response.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");
         response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,token");
         response.setHeader("Access-Control-Allow-Methods", "GET, HEAD, POST, PUT, DELETE, TRACE, OPTIONS, PATCH");
